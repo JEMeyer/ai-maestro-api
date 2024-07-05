9667576110648da676cf181b29417e1fa305775b
Joe Meyer <joseph@meyer.id>
2024-07-05 18:35:25 -0500

    better cors

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,26 @@ const server = createServer(app);
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Enable CORS
-app.use(cors());
+// CORS configuration
+const corsOrigins = process.env.ALLOWED_CORS_ORIGINS?.split(',') ?? [];
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean) => void
+  ) {
+    const allowedOrigins = ['http://localhost:5173', ...corsOrigins];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
 
 app.use('/api/computers', computersRouter);
 app.use('/api/gpus', gpusRouter);
