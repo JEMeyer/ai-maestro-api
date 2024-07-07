5c37509c437e9ba853bafbaf55d15ce213651ae0
Joe Meyer <joseph@meyer.id>
2024-07-07 01:22:40 -0500

    let any localhost through

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,11 @@ const corsOptions = {
     origin: string | undefined,
     callback: (error: Error | null, allow?: boolean) => void
   ) {
-    const allowedOrigins = ['http://localhost:5173', ...corsOrigins];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (
+      !origin ||
+      corsOrigins.indexOf(origin) !== -1 ||
+      origin.startsWith('http://localhost:')
+    ) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
