2894ce4054cba3e75ff827668664962c35bea8fb
Joe Meyer <joseph@meyer.id>
2024-07-10 12:23:06 -0500

    drain stuff

--- src/services/sse.ts
+++ src/services/sse.ts
@@ -6,19 +6,35 @@ export const setupSSE = (app: Express) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no');
 
     res.flushHeaders();
 
     res.write(': init\n\n');
 
-    // Send heartbeat every 10 seconds to keep the connection open
+    // Send heartbeat every 15 seconds to keep the connection open
     const heartbeatInterval = setInterval(() => {
-      res.write(': ping\n\n');
-    }, 10000);
+      if (!res.write(': ping\n\n')) {
+        res.once('drain', () => {
+          console.log(
+            `[${new Date().toISOString()}] Heartbeat sent after drain`
+          );
+        });
+      }
+    }, 15000);
 
     const handleMessage = (message: string) => {
       console.log(`[${new Date().toISOString()}] Sending message: ${message}`);
-      res.write(`data: ${message}\n\n`);
+      if (!res.write(`data: ${message}\n\n`)) {
+        res.once('drain', () => {
+          console.log(
+            `[${new Date().toISOString()}] Message sent after drain: ${message}`
+          );
+          res.write(': ping\n\n'); // Send a ping to ensure the message is flushed
+        });
+      } else {
+        res.write(': ping\n\n'); // Send a ping to ensure the message is flushed
+      }
     };
 
     pubSubClient.subscribe('gpu-lock-changes', (message) => {
